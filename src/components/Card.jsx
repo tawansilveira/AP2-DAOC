6bca233094e271c37844b47636176ba35b2d043f
GustavoJST <gutojusto22@gmail.com>
2024-10-21 15:13:56 -0300

    Equaliza tamanho das imagens entre os cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,10 +5,12 @@ const Card = ({ pessoa }) => {
     <div
     className='card text-center bg-gray-900  rounded-2xl p-2 shadow-md
     transform-gpu hover:-translate-y-1 transition hover:ease-in-out hover:drop-shadow-md'>
-      <img
-      className='rounded-2xl shadow-lg shadow-gray-950'
-      src={pessoa.foto} alt={pessoa.nome} />
-      <h3 className='text-3 font-semibold mt-1'>{pessoa.nome}</h3>
+      <div className='card-container aspect-square'>
+        <img
+        className='card-image rounded-2xl shadow-lg shadow-gray-950 h-full w-full'
+        src={pessoa.foto} alt={pessoa.nome} />
+      </div>
+      <h3 className='text-3 font-semibold mt-1 leading-tight mb-2'>{pessoa.nome}</h3>
       <p className='text-sm'>
         {pessoa.egresso ? 'Egresso/Convidado' : 'Estudante'}
       </p>
@@ -18,5 +20,6 @@ const Card = ({ pessoa }) => {
     </div>
   );
 };
+
 
 export default Card;
