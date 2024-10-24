cbbb55457e216e593d256f838f8108a572dc14a4
GustavoJST <gutojusto22@gmail.com>
2024-10-23 21:31:46 -0300

    Adiciona flexbox no card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ pessoa }) => {
   return (
     <div
-    className='card text-center bg-gray-900  rounded-2xl p-2 shadow-md
+    className='card text-center bg-gray-900 rounded-2xl p-2 shadow-md
     transform-gpu hover:-translate-y-1 transition hover:ease-in-out hover:drop-shadow-md'>
       <div className='card-container aspect-square'>
         <img
@@ -11,12 +11,14 @@ const Card = ({ pessoa }) => {
         src={pessoa.foto} alt={pessoa.nome} />
       </div>
       <h3 className='text-xl md:text-sm font-semibold mt-1 leading-tight mb-2'>{pessoa.nome}</h3>
-      <p className='text-sm'>
-        {pessoa.egresso ? 'Egresso/Convidado' : 'Estudante'}
-      </p>
-      <p className='text-sm'>
-        {(pessoa.pago ? 'Confirmado' : 'Não Confirmado')}
-      </p>
+      <div className='flex flex-col flex-grow'>
+          <p className='text-sm'>
+            {pessoa.egresso ? 'Egresso/Convidado' : 'Estudante'}
+          </p>
+          <p className='text-sm'>
+            {(pessoa.pago ? 'Confirmado' : 'Não Confirmado')}
+          </p>
+      </div>
     </div>
   );
 };
